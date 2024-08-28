2c76eea1de91fa578e91f954ca738071027a8dd3
Yuli <yu.expert@gmail.com>
2024-08-28 12:40:58 +0600

    add task 4

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,19 @@
+const formEl = document.querySelector("form.login-form");
+formEl.addEventListener("submit", processLogin);
 
+function processLogin(event) {
+    event.preventDefault();
+    const form = event.target;
+    const email = form.elements.email.value.trim();
+    const password = form.elements.password.value.trim();
+    let userData = {};
+    
+    if (email === "" || password === "") {
+        return alert("All form fields must be filled in");
+    };
+    
+    userData.email = email;
+    userData.password = password;
+    console.log("userData:", userData)
+    form.reset();
+};
