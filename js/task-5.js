db11dba4a7842a51b305ed13da23dde8dbdc416a
Yuli <yu.expert@gmail.com>
2024-08-28 15:51:16 +0600

    add task 5

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,12 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const bodyColor = document.querySelector("body");
+const buttonEl = document.querySelector(".change-color");
+const spanEL = document.querySelector(".color");
+
+buttonEl.addEventListener("click", () => {
+  bodyColor.style.backgroundColor = getRandomHexColor();
+  spanEL.style.color = bodyColor.style.backgroundColor;
+});
