361505163cd483d8f49d3d6920fb7f2b6ae3eb1c
Yuli <yu.expert@gmail.com>
2024-08-29 18:52:12 +0600

    add task 6

--- js/task-6.js
+++ js/task-6.js
@@ -3,3 +3,33 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const inputEl = document.querySelector("input");
+const buttonCreateEl = document.querySelector("button[data-create]");
+const buttonDestroyEl = document.querySelector("button[data-destroy]");
+const boxesEl = document.querySelector("#boxes");
+
+function createBoxes(amount) {
+  boxesEl.innerHTML = "";
+  let size = 30;
+  for(let i = 0; i < amount; i++) {
+    const box = document.createElement("div");
+    box.style.width = `${size}px`;
+    box.style.height = `${size}px`;
+    box.style.backgroundColor = getRandomHexColor();
+    boxesEl.appendChild(box);
+    size += 10;
+  };
+}
+
+buttonCreateEl.addEventListener("click", () => {
+  const amount = inputEl.value;
+  if (amount <= 100) {
+    createBoxes(amount);
+    inputEl.value = "";
+  };
+})
+
+buttonDestroyEl.addEventListener("click", () => {
+  boxesEl.innerHTML = "";
+})
